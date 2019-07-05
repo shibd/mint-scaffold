f8a6ae7a163839ea8e7ff00c013889876e9f9440
bdshi <bdshi@dfocuspace.com>
2019-07-05 16:46:23 +0800

    调整脚本

--- src/main/resources/META-INF/archetype-post-generate.groovy
+++ src/main/resources/META-INF/archetype-post-generate.groovy
@@ -10,3 +10,13 @@ upgradeVersion.setExecutable(true, false)
 def mybatisGenerator = new File( request.getOutputDirectory(), request.getArtifactId() + "/tools/mybatis-generator")
 println "mybatis-generator file path:" + mybatisGenerator;
 mybatisGenerator.setExecutable(true, false)
+
+def buildStartup = new File( request.getOutputDirectory(), request.getArtifactId() + "/tools/build-startup")
+println "build-startup file path:" + buildStartup;
+buildStartup.setExecutable(true, false)
+
+
+
+
+
+print ""
