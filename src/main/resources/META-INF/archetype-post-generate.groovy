e23e48fc5334d71de4f56a887002f0b6ac020e75
bdshi <bdshi@dfocuspace.com>
2019-07-05 15:15:07 +0800

    增加维护一些可执行脚本

---
+++ src/main/resources/META-INF/archetype-post-generate.groovy
@@ -0,0 +1,12 @@
+// 设置脚本可执行权限
+def deployMaven = new File( request.getOutputDirectory(), request.getArtifactId() + "/tools/deploy-maven")
+println "deploy-maven file path:" + deployMaven
+deployMaven.setExecutable(true, false)
+
+def upgradeVersion = new File( request.getOutputDirectory(), request.getArtifactId() + "/tools/upgrade-version")
+println "upgrade-version file path:" + upgradeVersion;
+upgradeVersion.setExecutable(true, false)
+
+def mybatisGenerator = new File( request.getOutputDirectory(), request.getArtifactId() + "/tools/mybatis-generator")
+println "mybatis-generator file path:" + mybatisGenerator;
+mybatisGenerator.setExecutable(true, false)
