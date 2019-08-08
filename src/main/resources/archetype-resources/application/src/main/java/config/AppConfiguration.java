ec213ba028896f7289662b7d955b7e7373782950
bdshi <bdshi@dfocuspace.com>
2019-08-08 18:26:12 +0800

    feat: 增加swagger配置,过滤掉facade的api

--- src/main/resources/archetype-resources/application/src/main/java/config/AppConfiguration.java
+++ src/main/resources/archetype-resources/application/src/main/java/config/AppConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
